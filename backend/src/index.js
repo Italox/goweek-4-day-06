4a60807cb6cb4fb75c12f4a074bc569945a0dd4f
stefanowolfs <stefanowolfs@gmail.com>
2018-11-07 00:02:45 -0200

    aula 2

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,21 +1,41 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 
 const app = express();
 
+//extrai servidor do express
+const server = require('http').Server(app);
+
+// habilita servidor a escutar protocolo AWS (pra entender req real-time)
+// este objeto contem a informação de todos os user que estão usando
+// a aplicação
+const io = require('socket.io')(server);
+
 mongoose.connect('mongodb://goweek:123ksword@ds155313.mlab.com:55313/goweek-stefano',
-    { useNewUrlParser: true }
+    {
+        useNewUrlParser: true
+    }
 );
 
-/*
-app.get('/', (req, res) => {
-    return res.send(':) Helo World');
+// para que eu torne a variavel "io" visivel para toda aplicação
+// vou criar um middleware
+app.use((req, res, next) => {
+    // ao recever req cria esta variavel
+    req.io = io;
+
+    // e retorna ao funcionamento normal
+    return next();
 });
-*/
 
-app.use(require('./routes'))
 
-app.listen(3000, () => {
+// diz que vou usar json em todas req..
+app.use(cors()); // permite outras applicações usarem o server
+app.use(express.json());
+app.use(require('./routes'));
+
+//com acesso ao protocolo aws
+server.listen(3000, () => {
     console.log('Server started on port 3000');
 });
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,6 +2,12 @@ const express = require('express');
 
 const routes = express.Router();
 
-// routes.get
+const TweetController = require('./controllers/TweetController');
+const LikeController = require('./controllers/LikeController');
+
+routes.get('/tweets', TweetController.index);
+routes.post('/tweets', TweetController.store);
+
+routes.post('/likes/:id', LikeController.store);
 
 module.exports = routes;
